7fed33ab7beb3fabedfad1f67494e674aa950c8b
jdgiese <johndgiese@gmail.com>
2012-08-19 18:55:01 -0400

    added two batch files

---
+++ win32fix.bat
@@ -0,0 +1,14 @@
+mt.exe -inputresource:C:\Windows\system32\python27.dll;#2 -outputresource:C:\Python27\Lib\site-packages\zmq\core\constants.pyd;#2
+mt.exe -inputresource:C:\Windows\system32\python27.dll;#2 -outputresource:C:\Python27\Lib\site-packages\zmq\core\context.pyd;#2
+mt.exe -inputresource:C:\Windows\system32\python27.dll;#2 -outputresource:C:\Python27\Lib\site-packages\zmq\core\device.pyd;#2
+mt.exe -inputresource:C:\Windows\system32\python27.dll;#2 -outputresource:C:\Python27\Lib\site-packages\zmq\core\error.pyd;#2
+mt.exe -inputresource:C:\Windows\system32\python27.dll;#2 -outputresource:C:\Python27\Lib\site-packages\zmq\core\message.pyd;#2
+mt.exe -inputresource:C:\Windows\system32\python27.dll;#2 -outputresource:C:\Python27\Lib\site-packages\zmq\core\socket.pyd;#2
+mt.exe -inputresource:C:\Windows\system32\python27.dll;#2 -outputresource:C:\Python27\Lib\site-packages\zmq\core\stopwatch.pyd;#2
+mt.exe -inputresource:C:\Windows\system32\python27.dll;#2 -outputresource:C:\Python27\Lib\site-packages\zmq\core\_poll.pyd;#2
+mt.exe -inputresource:C:\Windows\system32\python27.dll;#2 -outputresource:C:\Python27\Lib\site-packages\zmq\core\_version.pyd;#2
+mt.exe -inputresource:C:\Windows\system32\python27.dll;#2 -outputresource:C:\Python27\Lib\site-packages\zmq\devices\monitoredqueue.pyd;#2
+mt.exe -inputresource:C:\Windows\system32\python27.dll;#2 -outputresource:C:\Python27\Lib\site-packages\zmq\utils\initthreads.pyd;#2
+mt.exe -inputresource:C:\Windows\system32\python27.dll;#2 -outputresource:C:\Python27\Lib\site-packages\zmq\utils\rebuffer.pyd;#2
+mt.exe -inputresource:C:\Windows\system32\python27.dll;#2 -outputresource:C:\Python27\Lib\site-packages\zmq\libzmq.dll;#2
+PAUSE
